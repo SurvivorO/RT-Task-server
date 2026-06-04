5ce30de68d8eb3273913b05d5f1be2024d88d551
SurvivorO <harshitbansalboy@gmail.com>
2026-06-04 23:39:16 +0530

    added env package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import app from "./src/app"
+import app from "./src/app.js"
+import "dotenv/config"
 
-app.listen(3000, ()=> {
-    console.log('Server started at port 3000')
+app.listen(process.env.PORT, ()=> {
+    console.log(`Server started at port ${process.env.PORT}`)
 })
