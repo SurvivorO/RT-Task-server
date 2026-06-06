d15867f9367c04a4adedd65a07907aa1a64f6bca
SurvivorO <harshitbansalboy@gmail.com>
2026-06-06 11:05:24 +0530

    updated tests to include private '/health' endpoint

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import request from "supertest"
 import app from "../src/app"
 
@@ -9,5 +10,23 @@ describe('Basic Server Health Checck', () => {
         expect(response.status).toBe(200)
         expect(response.text).toBe("Hello world!")
 
+    })
+
+    it('should return 200 OK and healthy status on the /health endpoint with valid API key', async () => {
+        const response = await request(app)
+            .get('/health')
+            .set('x-api-key', process.env.INTERNAL_API_KEY)
+
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual({status: "healthy"})
+
+    })
+
+    it('should return 404 Not Found on the /health endpoint without API key', async () => {
+        const response = await request(app)
+            .get('/health')
+
+        expect(response.status).toBe(404)
+        expect(response.text).toBe("Cannot GET /health")
     })
 })
