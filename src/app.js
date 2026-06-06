45be0ec9bf9d1d162c0e5b9c113bc245dec3180a
SurvivorO <harshitbansalboy@gmail.com>
2026-06-06 12:06:41 +0530

    added use json middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import requireApikey from "./middlewares/requireApikey.js"
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/health', requireApikey, (req, res) => {
     res.status(200)
     res.json({status: "healthy"})    
