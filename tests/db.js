d41dc1ad9d4b9daa29ffe115e4e239cf7679d52a
SurvivorO <harshitbansalboy@gmail.com>
2026-06-06 12:07:00 +0530

    added a db helper for tests

---
+++ tests/db.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+import MongoMemoryServer from "mongodb-memory-server"
+
+let mongoServer
+
+const connectDB = async () => {
+    mongoServer = await MongoMemoryServer.create()
+    const uri = mongoServer.getUri()
+
+    await mongoose.connect(uri)
+}
+
+const disconnectDB = async () => {
+    if(mongoServer) {
+        await mongoose.connection.dropDatabase()
+        await mongoose.connection.close()
+        await mongoServer.stop()
+    }
+}
+
+const clearDB = async () => {
+    const collections = mongoose.connection.collections
+
+    for(const key in collections) {
+        await collections[key].deleteMany()
+    }
+}
+
+export {connectDB, disconnectDB, clearDB}
