f80fab4fe6d61a7c0aadc8a4ff56c21c397c420d
SurvivorO <harshitbansalboy@gmail.com>
2026-06-06 19:30:41 +0530

    changed connection method to use mongoose

--- src/database.js
+++ src/database.js
@@ -1,36 +1,36 @@
-import {MongoClient, Db, Collection} from "mongodb"
-
-let client
-let db
+import mongoose from "mongoose";
+import "dotenv/config"
 
 const connectDB = async () => {
-
-    if(db) {
-        return db
-    }
-
     const uri = process.env.MONGO_URI
-
-    if(!uri) {
-        throw new Error("MONGO_URI is not defined in environment variables")
+    if (!uri) {
+        console.error("MONGO_URI is not defined in environment variables")
+        process.exit(1)
     }
 
-    try{
-        client = new MongoClient(process.env.MONGO_URI)
-        await client.connect()
-        db = client.db(process.env.MONGO_DB_NAME)
-        console.log("Connected to MongoDB: ", db.databaseName)        
-        return db
-    }catch(err) { 
+    const dbName = process.env.MONGO_DB_NAME
+    if(!dbName) {
+        console.error("MONGO_DB_NAME is not defined in environment variables")
+        process.exit(1)
+    }
+
+    try {
+        await mongoose.connect(uri, {
+            dbName: dbName
+        })
+        console.log("Connected to MongoDB: ", dbName)
+    }catch(err) {
         console.error("Error connecting to MongoDB: ", err)
-        throw err
+        process.exit(1)
     }
 }
 
 const closeDB = async () => {
-    if(client) {
-        await client.close()
+    try {
+        await mongoose.connection.close()
         console.log("MongoDB connection closed")
+    }catch(err) {
+        console.error("Error closing MongoDB connection: ", err)
     }
 }
 
